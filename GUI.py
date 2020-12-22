bceff98e4e8f8923d9dd5e02f61bcdb0616e6117
MeletChirino <melet.chirino@gmail.com>
2020-12-22 17:43:33 +0100

    Primer prototipo de la interfaz grafica funcional, de aqui en adelante el resto es carpinteria y maquillaje para la aplicacion

--- GUI.py
+++ GUI.py
@@ -1,18 +1,33 @@
+# kivy libraries
 import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
+# Own libraries
 from python_interface.temoin import Temoin
+from python_interface.serial_ports import serial_ports
 
 class ConnectPage(Screen):
+    """Page used for connecting to serial port"""
     serial_port = ObjectProperty(None)
     baud_rate = ObjectProperty(None)
     boton = ObjectProperty(None)
 
+    def scan_serial_ports(self):
+        """
+        Method for finding available serial ports in system
+        """
+        print("Buscando Puertos Seriales")
+        serial_list = serial_ports()
+        self.serial_port.values = serial_list
+
+
     def pressing(self):
-        print("ke ricoolino")
+        """
+        Method for connecting to Serial port
+        """
         serial_port_ = self.serial_port.text
         baud_rate_ = self.baud_rate.text
         arduino.serial_port = serial_port_
@@ -29,8 +44,11 @@ class ConnectPage(Screen):
 
 class GetDataPage(Screen):
     def go_back(self):
-        print("delicioso")
+        """
+        Method for Getting data from Arduino
+        """
         arduino.get_data()
+        #Con los datos aqui en python podemos hacer graficos y la vaina
         #relay_app.root.current = "ConnectPage"
 
 class WindowManager(ScreenManager):
