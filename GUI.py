487317515c04c4581a32704532d20b3da4c58d04
MeletChirino <melet.chirino@gmail.com>
2020-12-22 10:32:09 +0100

    Ahora puedo modificar los archivos widgets del layout tranquilamente y ponerle funciones a los botones lo cual le da una funcionalidad a mi programa

--- GUI.py
+++ GUI.py
@@ -5,9 +5,15 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
 class MyGrid(Widget):
-    pass
+    serial = ObjectProperty(None)
+
+    def pressing(self):
+        print("press ", self.serial.text)
+        self.serial.text = ""
+
 
 class MainPage(GridLayout):
     def __init__(self, **kwargs):
